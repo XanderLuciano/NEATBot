873e221486ae8cd2b7274853420764ee20940e85
Xander Luciano <xanderluciano@gmail.com>
2018-07-30 07:11:03 -0700

    Need boost!

--- RLBotCSharpExample/RLBotCSharpExample/ExampleBot.cs
+++ RLBotCSharpExample/RLBotCSharpExample/ExampleBot.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using RLBotDotNet;
 using rlbot.flat;
+using g3;
+using MathNet.Numerics;
 
 namespace RLBotCSharpExample
 {
@@ -9,6 +11,48 @@ namespace RLBotCSharpExample
     {
         // We want the constructor for ExampleBot to extend from Bot, but we don't want to add anything to it.
         public ExampleBot(string botName, int botTeam, int botIndex) : base(botName, botTeam, botIndex) { }
+
+        private Random rnd = new Random();
+
+        private int targetBoostId = 0;
+        private bool needBoost = false;
+        private Vector3f[] BoostLocations = {
+            new Vector3f(-3072.0, -4096.0, 73.0),
+            new Vector3f(3072.0, -4096.0, 73.0),
+            new Vector3f(3584, 0, 0),
+            new Vector3f(-3584, 0, 0),
+            new Vector3f(-3072.0, 4096.0, 73.0),
+            new Vector3f(3072.0, 4096.0, 73.0),
+        };
+
+        private float CollectBoost(int boostId, Vector3 carPosition, Rotator carRotation)
+        {
+            Vector3f pos = new Vector3f(carPosition.X, carPosition.Y, carPosition.Z);
+            Vector3f target = BoostLocations[boostId];
+            double targetAngle = Math.Atan2(target.y - pos.y, target.x - pos.x);
+            double botAngle = targetAngle - carRotation.Yaw;
+            if (botAngle < -Math.PI)
+                botAngle += Math.PI * 2;
+            if (botAngle > Math.PI)
+                botAngle -= Math.PI * 2;
+
+            double angleToGo = botAngle / Math.PI;
+
+            return Math.Min(1, (Math.Max(-1, (float)angleToGo)));
+        }
+
+        public double getDistanceToBall(Vector3 carPosition, Vector3 ballPosition)
+        {
+            Vector3f car = new Vector3f(carPosition.X, carPosition.Y, carPosition.Z);
+            Vector3f ball = new Vector3f(ballPosition.X, ballPosition.Y, ballPosition.Z);
+            return car.Distance(ball);
+        }
+
+        public double getDistanceToPoint(Vector3 carPosition, Vector3f target)
+        {
+            Vector3f car = new Vector3f(carPosition.X, carPosition.Y, carPosition.Z);
+            return car.Distance(target);
+        }
 
         public override Controller GetOutput(GameTickPacket gameTickPacket)
         {
@@ -26,6 +70,8 @@ namespace RLBotCSharpExample
                 Vector3 carLocation = gameTickPacket.Players(this.index).Value.Physics.Value.Location.Value;
                 Rotator carRotation = gameTickPacket.Players(this.index).Value.Physics.Value.Rotation.Value;
 
+                PlayerInfo car = gameTickPacket.Players(this.index).Value;
+
                 // Calculate to get the angle from the front of the bot's car to the ball.
                 double botToTargetAngle = Math.Atan2(ballLocation.Y - carLocation.Y, ballLocation.X - carLocation.X);
                 double botFrontToTargetAngle = botToTargetAngle - carRotation.Yaw;
@@ -34,12 +80,47 @@ namespace RLBotCSharpExample
                     botFrontToTargetAngle += 2 * Math.PI;
                 if (botFrontToTargetAngle > Math.PI)
                     botFrontToTargetAngle -= 2 * Math.PI;
+                
+                controller.Boost = false;
 
-                // Decide which way to steer in order to get to the ball.
-                if (botFrontToTargetAngle > 0)
-                    controller.Steer = 1;
+                var steerAngle = 0f;
+
+                if (car.Boost == 0 && !needBoost)
+                {
+                    // Go to a random boost pad
+                    needBoost = true;
+                    targetBoostId = rnd.Next(BoostLocations.Length - 1);
+                    Console.WriteLine($"Car {this.index}: Need Boost!");
+                }
+                else if (car.Boost > 80)
+                {
+                    // Don't need boost, go for ball
+                    needBoost = false;
+                }
+
+                if (needBoost)
+                {
+                    // Looking for boost
+                    steerAngle = CollectBoost(targetBoostId, carLocation, carRotation);
+
+                    // Find new boost pad after we hit our target
+                    if (getDistanceToPoint(carLocation, BoostLocations[targetBoostId]) < 100)
+                        targetBoostId = rnd.Next(BoostLocations.Length - 1);
+
+                    // Walls tend to break things.
+                    if (carLocation.Z > 200)
+                        targetBoostId = rnd.Next(BoostLocations.Length - 1);
+                }
                 else
-                    controller.Steer = -1;
+                {
+                    // Go for ball!
+                    steerAngle = botFrontToTargetAngle > 0 ? 1 : -1;
+                }
+
+                // Boost towards ball!
+                controller.Boost = (getDistanceToBall(carLocation, ballLocation) < 2000);
+                
+                controller.Steer = steerAngle;
 
             }
             catch (Exception e)
